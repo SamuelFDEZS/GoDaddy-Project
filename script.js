ce0a65940921f136fe96e6e8a6fa77cc6c6b38a4
SamuelFDEZS <thggsamuel@gmail.com>
2024-06-06 02:28:16 +0200

    Add trusting section and body styles, add logic for trusting cards

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ tabs.forEach((tab, index) => {
         containers[index].classList.add('business__container--active');
     });
 });
+
+
+let buttons = gsap.utils.toArray('.trust__buttons__button');
+let cardsContainer = document.querySelector('.trust__services');
+let cardWidth = cardsContainer.querySelector('.trust__services__experience').offsetWidth;
+let cardsArray = Array.from(document.querySelectorAll('.trust__services__experience'));
+let cardsCount = cardsArray.length;
+let totalWidth = cardWidth * cardsCount;
+console.log(totalWidth)
+let displacement = cardWidth;
+let currentX = 0;
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        console.log(currentX)
+        if (index === 1 && currentX > -totalWidth + cardWidth * 3) {
+            currentX -= displacement;
+        } else if (index === 0 && currentX < 0) {
+            currentX += displacement;
+        }
+
+        gsap.to(cardsContainer, {
+            duration: 0.5,
+            x: currentX,
+            ease: 'power4.out',
+        });
+    });
+});
