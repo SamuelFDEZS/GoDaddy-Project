bdf9af04b4c383c7de16749b42697d9d0314db28
SamuelFDEZS <thggsamuel@gmail.com>
2024-07-18 02:13:21 +0200

    Add styles for mobile screens for header and search, add responsive styles for medium size screen on main section

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+// Variables Creation
+
 let tabs = Array.from(document.querySelectorAll('.business__tabs__tab'));
-
 let containers = Array.from(document.querySelectorAll('.business__container'));
+let buttons = gsap.utils.toArray('.trust__buttons__button');
+let cardsContainer = document.querySelector('.trust__services');
+let cardWidth = cardsContainer.querySelector('.trust__services__experience').offsetWidth;
+let cardsArray = Array.from(document.querySelectorAll('.trust__services__experience'));
+let cardsCount = cardsArray.length;
+let totalWidth = cardWidth * cardsCount;
+let displacement = cardWidth;
+let currentX = 0;
+let summary = Array.from(document.querySelectorAll('summary'));
 
+// Logic Implementation for Tabs, Buttons and Summary
 tabs.forEach((tab, index) => {
-    tab.addEventListener('click',() => {
+    tab.addEventListener('click', () => {
         tabs.map(tab => tab.classList.remove('business__tabs__tab--active'));
 
         containers.map(container => container.classList.remove('business__container--active'));
@@ -14,19 +25,10 @@ tabs.forEach((tab, index) => {
 });
 
 
-let buttons = gsap.utils.toArray('.trust__buttons__button');
-let cardsContainer = document.querySelector('.trust__services');
-let cardWidth = cardsContainer.querySelector('.trust__services__experience').offsetWidth;
-let cardsArray = Array.from(document.querySelectorAll('.trust__services__experience'));
-let cardsCount = cardsArray.length;
-let totalWidth = cardWidth * cardsCount;
-console.log(totalWidth)
-let displacement = cardWidth;
-let currentX = 0;
+
 
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
-        console.log(currentX)
         if (index === 1 && currentX > -totalWidth + cardWidth * 3) {
             currentX -= displacement;
         } else if (index === 0 && currentX < 0) {
@@ -39,4 +41,39 @@ buttons.forEach((button, index) => {
             ease: 'power4.out',
         });
     });
+});
+
+
+summary.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('open');
+    });
+})
+
+
+// Mobile-related Logic
+
+let dropdown = document.querySelector('.header__dropdown');
+let nav = document.querySelector('.header__nav');
+let body = document.querySelector('body');
+let xmark = document.querySelector('.header__nav__xmark');
+let filter = document.querySelector('.filter');
+
+dropdown.addEventListener('click', () => {
+    nav.classList.toggle('header__nav--visible');
+    body.classList.toggle('no-scroll');
+    filter.classList.toggle('filter--visible');
+});
+
+xmark.addEventListener('click', () => {
+    nav.classList.toggle('header__nav--visible');
+    body.classList.toggle('no-scroll');
+    filter.classList.toggle('filter--visible');
+});
+
+
+window.addEventListener('resize', () => {
+    nav.classList.remove('header__nav--visible');
+    body.classList.remove('no-scroll');
+    filter.classList.remove('filter--visible');
 });
